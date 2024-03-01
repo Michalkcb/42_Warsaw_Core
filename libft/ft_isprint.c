34f1755081839489131299c6af0ffd796155a70b
Michalkcb <michalkcb@gmail.com>
2024-03-01 18:07:46 +0100

    add isdigit

---
+++ libft/ft_isprint.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 18:26:19 by mbany             #+#    #+#             */
+/*   Updated: 2024/02/29 18:33:28 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_isprint(int c)
+{
+	return (c >= ' ' && c <= '~');
+}
+
+// int main()
+// {
+//     char test_char = 'A';
+//     if (ft_isprint(test_char))
+//         printf("Character '%c' is printable.\n", test_char);
+//     else
+//         printf("Character '%c' is not printable.\n", test_char);
+//     return 0;
+// }
