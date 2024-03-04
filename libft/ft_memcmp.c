2431a987612225a0aefb46767da1ad0a20a3ca10
Michalkcb <michalkcb@gmail.com>
2024-03-04 17:48:08 +0100

    add memcmp

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 15:26:17 by mbany             #+#    #+#             */
+/*   Updated: 2024/03/04 13:40:49 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_memcmp(void const *s1, void const *s2, size_t n)
+{
+	while (n--)
+	{
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		s1 = (unsigned char *)s1 + 1;
+		s2 = (unsigned char *)s2 + 1;
+
+	}
+	return (0);
+}
+
+// int main(void)
+// {
+//     char str1[] = "123456";
+//     char str2[] = "123a45";
+//     size_t n = 4;
+//     int result = ft_memcmp(str1, str2, n);
+
+//     printf("Comparison result: %d\n", result);
+//     return 0;
+// }
