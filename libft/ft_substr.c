9143a33339730cde02aafd910372f2dd84eb0c11
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-10 14:56:02 +0100

    add substr + norm

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,19 +6,69 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 12:01:27 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/10 12:08:04 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/10 14:52:25 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+#include "libft.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	s_len;
 	size_t	d_len;
-	int	i;
-	char *dest;
+	int		i;
+	char	*dest;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	if ((start > s_len))
+		return (0);
+	else if (s_len - start > len)
+		d_len = len;
+	else
+		d_len = s_len - start;
+	dest = (char *)malloc((d_len + 1) * sizeof(char));
+	if (!dest)
+		return (0);
+	while (i < d_len)
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
 }
 
-int main()
-{
-	
-}
+// int main()
+// {
+//     const char *original = "This is a test string.";
+//     unsigned int start = 5;
+//     size_t len = 6;
+
+//     char *substring = ft_substr(original, start, len);
+
+//     if (substring)
+//     {
+//         printf("Original string: %s\n", original);
+//         printf("Substring: %s\n", substring);
+//         free(substring);
+//     }
+//     else
+//     {
+//         printf("Memory allocation failed.\n");
+//     }
+
+//     return 0;
+// }
