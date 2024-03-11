609f91e8d617e45d4fc786ec291133a2eb7a05f0
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-11 12:10:14 +0100

    update strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 11:40:52 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/11 10:51:50 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/11 12:09:30 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,35 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
+char	*ft_strchr(char const *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		*str++;
+	}
+	if (*str == (char)c)
+		return ((char *)str);
+	else
+		return (NULL);
+}
+
+size_t ft_strlcpy(char *d, const char *s, size_t n)
+{
+    size_t  i;
+    i = 0;
+
+    while (s[i])
+        i++;
+    if (n == 0)
+        return (i);
+    while (--n && *s)
+        *d++ = *s++;
+    *d = '\0';
+    return (i);
+}
+
 char *ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
@@ -47,10 +76,15 @@ char *ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	end = ft_strlen(s) -1;
 	
-
-
-
-	return (dest)
+while (s1[start] && ft_strchr(set, s1[start]))
+	start++;
+while (s1[end] && ft_strchr(set, s1[end]) && end >= start)
+	start++;
+dest = (char *)malloc(sizeof(char) * ((end - start + 1) + 1))
+if (!dest)
+	return (0);
+ft_strlcpy(dest, s1 + start, (end - start + 1) + 1);
+return (dest)
 }
 
 int main()
