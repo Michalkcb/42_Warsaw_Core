01934573848f3e69554da2a596bf80f48e2c3ac8
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-11 16:20:48 +0100

    itoa done + norm

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 11:41:49 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/11 13:51:51 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/11 16:17:49 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,72 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static int len(int i)
-
-char *ft_itoa(int n)
+static int	nbr_len(int nbr)
 {
+	int	len;
 
+	if (nbr <= 0)
+		len = 1;
+	else
+		len = 0;
+	while (nbr != 0)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
 }
 
-int main()
+static void	handle_special(char *str, int *n, int *len)
 {
-	
+	if (*n == -2147483648)
+	{
+		str[(*len)--] = '8';
+		*n /= 10;
+	}
+	else if (!*n)
+		str[0] = '0';
+	if (*n < 0)
+	{
+		str[0] = '-';
+		*n *= -1;
+	}
 }
+
+char	*ft_itoa(int n)
+{
+	int		len;
+	char	*str;
+
+	len = nbr_len(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	str[len--] = 0;
+	handle_special(str, &n, &len);
+	while (n)
+	{
+		str[len--] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	int num1 = -12345;
+// 	int num2 = 6789;
+
+// 	char *str1 = ft_itoa(num1);
+// 	char *str2 = ft_itoa(num2);
+
+// 	// Testujemy wyniki
+// 	printf("Num1: %s\n", str1);
+// 	printf("Num2: %s\n", str2);
+
+// 	free(str1);
+// 	free(str2);
+
+// 	return 0;
+// }
