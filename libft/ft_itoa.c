9c77d7e14281b27b2c51ad7f936036ab703d671e
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-11 13:52:44 +0100

    init itoa

---
+++ libft/ft_itoa.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 11:41:49 by mbany             #+#    #+#             */
+/*   Updated: 2024/03/11 13:51:51 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Turn in files -
+// Parameters n: the integer to convert.
+// Return value The string representing the integer.
+// NULL if the allocation fails.
+// External functs. malloc
+// Description Allocates (with malloc(3)) and returns a string
+// representing the integer received as an argument.
+// Negative numbers must be handled.
+
+#include "libft.h"
+#include <stdlib.h>
+
+static int len(int i)
+
+char *ft_itoa(int n)
+{
+
+}
+
+int main()
+{
+	
+}
