cca5253fc95bcde2b07bf53c8719a1021eab6667
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-14 11:54:06 +0100

    norm + make

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,38 +6,35 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:15:31 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/02 15:44:04 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/14 11:43:43 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 char	*ft_strrchr(char const *str, int c)
 {
-    const char *last;
-    last = NULL;
+	const char	*last;
 
-    while (*str)
-        {
-            if (*str == (char)c)
-                last = str;
-            *str++;
-        }
-    
-
-    if (*str == (char)c)
-        return ( (char *)str);
-    
-    return((char *)last);
+	last = NULL;
+	while (*str)
+	{
+		if (*str == (char)c)
+			last = str;
+		str++;
+	}
+	if (*str == (char)c)
+		return ((char *)str);
+	return ((char *)last);
 }
 
-int main(void)
-{
-    char str[] = "abc";
-    char *result = ft_strrchr(str, 'a');
-    if (result != NULL)
-        printf( "found b on position %ld\n", result - str);
-    else
-        printf( "not found b on position on any position\n");
+// int main(void)
+// {
+//     char str[] = "abc";
+//     char *result = ft_strrchr(str, 'a');
+//     if (result != NULL)
+//         printf( "found b on position %ld\n", result - str);
+//     else
+//         printf( "not found b on position on any position\n");
 
-}
+// }
