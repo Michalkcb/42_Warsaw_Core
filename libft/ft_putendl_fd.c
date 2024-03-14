d53b5906aa5177aaa1bdff9c7fcd4c94274f6afa
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-14 11:50:29 +0100

    norm + make

---
+++ libft/ft_putendl_fd.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 11:49:17 by mbany             #+#    #+#             */
+/*   Updated: 2024/03/14 09:52:47 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Turn in files -
+// Parameters s: The string to output.
+// fd: The file descriptor on which to write.
+// Return value None
+// External functs. write
+// Description Outputs the string ’s’ to the given file descriptor
+// followed by a newline.
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
+
+// int main(void)
+// {
+//     int fd = 1;
+//     char *str = "Hello, world!\n";
+//     ft_putendl_fd(str, fd);
+//     return (0);
+// }
