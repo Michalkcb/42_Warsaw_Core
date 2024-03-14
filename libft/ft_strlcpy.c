6005d5cb41667ac477883e22bcaeb5b5392dcd8c
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-14 11:52:42 +0100

    norm + make

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,21 +6,32 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:47:07 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/02 16:16:36 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/14 09:35:10 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-strlcpy(char *d, const char *s, size_t n)
+#include "libft.h"
+
+size_t	ft_strlcpy(char *d, const char *s, size_t n)
 {
-    size_t  i;
-    i = 0;
+	size_t	i;
 
-    while (*src[i])
-        i++;
-    if (n != 0)
-        return (i)
-
-        /*trzeba dokończyć*/
-
-
+	i = 0;
+	while (s[i])
+		i++;
+	if (n == 0)
+		return (i);
+	while (--n && *s)
+		*d++ = *s++;
+	*d = '\0';
+	return (i);
 }
+
+// int main (void)
+// {
+//     char s[] = "12345";
+//     char d[10];
+//     size_t result = ft_strlcpy(d,s, sizeof(d));
+//     printf("skopiowany tekst: %s\n", d);
+//     printf("długość skopiowanego tekstu: %zu\n", result);
+// }
