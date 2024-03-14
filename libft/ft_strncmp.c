a9f6576e16e3ff93e4a592d9c16b25cdf82ec1af
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-14 11:53:42 +0100

    norm + make

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,32 +6,30 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 14:53:59 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/01 11:22:16 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/14 10:05:49 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
 
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] == s2[i] && s1[i] != 0 && i < n -1)
-	{
+	while (s1[i] == s2[i] && s1[i] != 0 && i < n - 1)
 		i++;
-	}
 	return (s1[i] - s2[i]);
 }
 
 //#include <stdio.h>
-//
 // int main(void)
 // {
 //     char str1[] = "Georgia";
 //     char str2[]= "Utah";
 //     int result;
-
 //     printf("%s\n", str1);
 //     result = ft_strncmp(str1, str2, 1);
 //     printf("%d\n", result);
