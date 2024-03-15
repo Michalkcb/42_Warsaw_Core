54c9781f526307bb3d62273cd468c10045fd1883
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-15 12:05:36 +0100

    tests

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -14,14 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
-	if (n == 0)
+	if (!n)
 		return (0);
 	while (s1[i] == s2[i] && s1[i] != 0 && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 //#include <stdio.h>
