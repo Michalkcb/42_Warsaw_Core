afd9e597b18906a453581cefc366395ee2ef15fd
Michalkcb <michalkcb@gmail.com>
2024-03-17 19:45:59 +0100

    putendl

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -19,12 +19,6 @@
 // followed by a newline.
 #include "libft.h"
 
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	write(fd, s, ft_strlen(s));
-}
-
 void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
