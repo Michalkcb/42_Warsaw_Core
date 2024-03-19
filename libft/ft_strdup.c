23b7f197524df74af5e96715f56379c96bf48270
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-19 09:59:14 +0100

    tests pass

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,23 +6,13 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 09:39:26 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/14 09:47:48 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/18 11:03:07 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <stdlib.h>
 // #include <stdio.h>
 #include "libft.h"
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != 0)
-		i++;
-	return (i);
-}
 
 char	*ft_strdup(const char *s)
 {
