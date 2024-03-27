91b9224b442c5abbfd210e0d8a79bcc1faa18aa0
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-27 13:59:58 +0100

    update

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,8 +6,50 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 12:52:39 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/25 15:36:25 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/27 13:53:05 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prossi <prossi@student.42adel.org.au>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 12:20:51 by prossi            #+#    #+#             */
+/*   Updated: 2022/09/05 13:52:52 by prossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		i;
+	int		len;
+	char	*dest;
+
+	i = 0;
+	len = ft_strlen(s);
+	dest = malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (0);
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
