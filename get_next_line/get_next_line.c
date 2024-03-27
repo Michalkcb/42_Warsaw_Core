fcfc856c076b0feea36ecb18051b319f1356a2b3
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-03-27 13:59:21 +0100

    update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,122 +6,86 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:00:52 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/26 10:55:27 by mbany            ###   ########.fr       */
+/*   Updated: 2024/03/27 13:58:31 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // /=∂/\/\[](_)§ /\/\@|V †|-|@¯|¯ /-/!570®1<|-\£1_`/ ¢@/\/\ε vv!7}{ ???
-// FAMOUS MAN THAT HISTORICALLY CAME WITH
-
+// # include <stdlib.h>
+// # include <unistd.h>
+// #include <stdio.h>
+// #include <fcntl.h>
+// #ifndef BUFFER_SIZE
+// #define BUFFER_SIZE 42
+// #endif
+// int	ft_strlen(char *s)
+// {
+// 	int	i;
+// 	i = 0;
+// 	while (s[i] != 0)
+// 		i++;
+// 	return (i);
+// }
+// char	*ft_strdup(char *s)
+// {
+// 	int		i;
+// 	int		len;
+// 	char	*dest;
+// 	i = 0;
+// 	len = ft_strlen(s);
+// 	dest = malloc(sizeof(char) * (len + 1));
+// 	if (!dest)
+// 		return (0);
+// 	while (s[i])
+// 	{
+// 		dest[i] = s[i];
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
+/////
+/////
 #include "get_next_line.h"
-
-int	ft_strlen(char *s)
-{
-	int	i;
-
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i] != '\0' && s[i] != '\n')
-		i++;
-	if (s[i] == '\n')
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	int		len;
-	int		i;
-	char	*new_str;
-
-	len = ft_strlen(s1) + ft_strlen(s2);
-	i = 0;
-	new_str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!new_str)
-		return (free(s1), NULL);
-	while (s1 && s1[i])
-	{
-		new_str[i] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (s2 && s2[i])
-	{
-		new_str[len - ft_strlen(s2) + i] = s2[i];
-		if (s2[i] == '\n')
-			break ;
-		i++;
-	}
-	new_str[len] = '\0';
-	free(s1);
-	return (new_str);
-}
-
-int	update_buffer(char *buf)
-{
-	int	flag;
-	int	i;
-	int	j;
-
-	flag = 0;
-	i = 0;
-	j = 0;
-	while (buf[i])
-	{
-		if (flag)
-			buf[j++] = buf[i];
-		if (buf[i] == '\n')
-			flag = 1;
-		buf[i++] = '\0';
-	}
-	return (flag);
-}
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	int			bytes;
-	char		*line;
+	char	buffer;
+	char	line[8000000];
+	int		b;
+	int		i;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = NULL;
-	bytes = 0;
-	bytes = read(fd, buffer, BUFFER_SIZE);
-	while (buffer[0] || bytes > 0)
+	i = 0;
+	b = read(fd, &buffer, 1);
+	while (b > 0)
 	{
-		line = ft_strjoin(line, buffer);
-		if (!line)
-			return (NULL);
-		if (update_buffer(buffer))
+		line[i++] = buffer;
+		if (buffer == '\n')
 			break ;
-		bytes = read(fd, buffer, BUFFER_SIZE);
+		b = read(fd, &buffer, 1);
 	}
-	if (line && bytes < 0)
-	{
-		free(line);
-		line = NULL;
-	}
-	return (line);
+	line[i] = '\0';
+	if (b <= 0 && i == 0)
+		return (NULL);
+	return (ft_strdup(line));
 }
 
-// int main()
+// int	main(void)
 // {
-// 	int fd;
-// 	char *line;
-
-// 	fd = open("file1.txt", O_RDONLY);
-// 	if (fd == -1)
+// 	char	*line;
+// 	int		i;
+// 	int		fd1;
+// 	fd1 = open("test.txt", O_RDONLY);
+// 	i = 1;
+// 	while (i < 500)
 // 	{
-// 		perror("open");
-// 		return 1;
-// 	}
-// 	while ((line = get_next_line(fd)) != NULL)
-// 	{
-// 		printf("%s\n", line);
+// 		line = get_next_line(fd1);
+// 		printf("line [%02d]: %s", i, line);
 // 		free(line);
+// 		i++;
 // 	}
-// 	close(fd);
-// 	return 0;
+// 	close(fd1);
+// 	return (0);
 // }
