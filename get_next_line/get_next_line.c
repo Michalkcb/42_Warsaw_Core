b86c8337d2d0da85cb432cef08d408d04ee92e16
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-04-04 18:25:32 +0200

    new

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,151 +1,124 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 13:00:52 by mbany             #+#    #+#             */
-/*   Updated: 2024/04/04 13:56:01 by mbany            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-//=∂/\/\[](_)§ /\/\@|V †|-|@¯|¯ /-/!570®1<|-\£1_`/ ¢@/\/\ε vv!7}{ ???
-
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
+# define BUFFER_SIZE 42
 #endif
-int	ft_strlen(char *s)
+# ifndef MAX_FD
+#  define MAX_FD 1048
+# endif
+
+// #include "get_next_line.h"  // Dołączenie pliku nagłówkowego get_next_line.h, który zawiera deklaracje funkcji i stałe.
+
+// Funkcja obliczająca długość ciągu znaków, uwzględniając znak nowej linii.
+int ft_strlen(char *s)
 {
-	int	i;
-	i = 0;
-	while (s[i] != 0)
-		i++;
-	return (i);
-}
-char	*ft_strdup(char *s)
-{
-	int		i;
-	int		len;
-	char	*dest;
-	i = 0;
-	len = ft_strlen(s);
-	dest = malloc(sizeof(char) * (len + 1));
-	if (!dest)
-		return (0);
-	while (s[i])
-	{
-		dest[i] = s[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+  int i = 0;  // Inicjalizacja licznika.
+
+  if (!s)  // Sprawdzenie, czy wskaźnik na ciąg znaków jest pusty.
+    return (0);  // Jeśli wskaźnik jest pusty, zwróć 0.
+  while (s[i] != '\0' && s[i] != '\n') // Pętla while iterująca po ciągu znaków do momentu znalezienia znaku nowej linii lub końca ciągu.
+    i++;  // Inkrementacja licznika.
+  if (s[i] == '\n') // Jeśli znaleziono znak nowej linii, zwiększ długość o 1.
+    i++;
+  return (i);  // Zwróć długość ciągu znaków.
 }
 
-char	*ft_strjoin(char *s1, char *s2)
+// Funkcja łącząca dwa ciągi znaków w jeden.
+char  *ft_strjoin(char *s1, char *s2)
 {
-	int		i;
-	int		j;
-	int		s1_len;
-	int		s2_len;
-	char	*dest;
+  int   len = ft_strlen(s1) + ft_strlen(s2); // Obliczenie długości nowego ciągu.
+  int   i = 0;  // Inicjalizacja licznika dla pierwszego ciągu znaków.
+  int   j = 0;  // Inicjalizacja licznika dla drugiego ciągu znaków.
+  char  *new_str;  // Deklaracja wskaźnika na nowy ciąg znaków.
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	dest = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		dest[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		dest[i + j] = s2[j];
-		j++;
-	}
-	dest[i + j] = 0;
-	return (dest);
+  new_str = (char *)malloc(sizeof(char) * (len + 1));  // Alokacja pamięci dla nowego ciągu znaków.
+  if (!new_str)  // Sprawdzenie, czy alokacja pamięci zakończyła się sukcesem.
+    return (free(s1), NULL);  // Zwolnienie pamięci zaalokowanej na pierwszy ciąg znaków i zwrócenie NULL.
+  while (s1 && s1[i]) // Pętla kopiująca znaki z pierwszego ciągu do nowego.
+  {
+    new_str[i] = s1[i];
+    i++;  // Inkrementacja licznika.
+  }
+  while (s2 && s2[j]) // Pętla kopiująca znaki z drugiego ciągu do nowego, zatrzymująca się w przypadku znaku nowej linii.
+  {
+    new_str[i] = s2[j];
+    if (new_str[i] == '\n')
+      break ;
+    i++;
+    j++;  // Inkrementacja licznika.
+  }
+  new_str[len] = '\0'; // Dodanie znaku końca ciągu.
+  free(s1);  // Zwolnienie pamięci zaalokowanej na pierwszy ciąg znaków.
+  return (new_str);  // Zwróć nowy ciąg znaków.
 }
 
-char	*ft_strchr(char const *str, int c)
+// Funkcja aktualizująca bufor, usuwając odczytane znaki.
+int update_buffer(char *buf)
 {
-	while (*str)
-	{
-		if (*str == (char)c)
-			return ((char *)str);
-		str++;
-	}
-	if (*str == (char)c)
-		return ((char *)str);
-	else
-		return (NULL);
+  int flag = 0;  // Inicjalizacja flagi.
+  int i = 0;  // Inicjalizacja licznika dla bufora.
+  int j = 0;  // Inicjalizacja licznika dla bufora po aktualizacji.
+
+  while (buf[i]) // Pętla while iterująca po buforze.
+  {
+    if (flag)  // Jeśli flaga jest ustawiona, przenieś znaki do początku bufora.
+      buf[j++] = buf[i];
+    if (buf[i] == '\n')  // Sprawdź, czy znaleziono znak nowej linii.
+      flag = 1;  // Ustaw flagę na 1.
+    buf[i++] = '\0';  // Zastąp znaki w buforze znakiem null.
+  }
+  return (flag);  // Zwróć flagę, informującą o znalezieniu znaku nowej linii.
 }
 
-#include "get_next_line.h"
-
-char	*get_next_line(int fd)
+// Główna funkcja get_next_line do odczytu kolejnych linii z pliku.
+char  *get_next_line(int fd)
 {
-	char	*buffer;
-	char	*line;
-	int		b;
-	int		i;
+  static char buffer[MAX_FD][BUFFER_SIZE + 1];  // Bufor statyczny przechowujący odczytane dane.
+  int         bytes;  // Liczba odczytanych bajtów.
+  char        *line;  // Wskaźnik na aktualną linię.
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return (NULL);
-	line = ft_strdup("");
-	if (!line)
-	{
-		free(buffer);
-		return (NULL);
-	}	
-	b = read(fd, buffer, BUFFER_SIZE);
-	while (b > 0)
-	{
-		buffer[b] = '\0';
-		line = ft_strjoin(line, buffer);
-		if (!line)
-		{
-			free(buffer);
-			return (NULL);
-		}	
-		if (ft_strchr(buffer,'\n'))
-			break ;
-		b = read(fd, buffer, BUFFER_SIZE);
-	}
-	free(buffer);	
-	if (b <= 0 && ft_strlen(line) == 0)
-		{
-			free(line);
-			return (NULL);
-		}
-	return (line);
+  if (fd < 0 || fd > MAX_FD || BUFFER_SIZE <= 0)  // Sprawdzenie poprawności parametrów.
+    return (NULL);  // Zwrócenie NULL w przypadku błędu.
+  line = NULL;  // Inicjalizacja wskaźnika linii.
+  bytes = 0;  // Inicjalizacja liczby odczytanych bajtów.
+  while (buffer[fd][0] || (bytes = read(fd, buffer[fd], BUFFER_SIZE)) > 0)
+  {
+    line = ft_strjoin(line, buffer[fd]);  // Łączenie linii z odczytanymi danymi z bufora.
+    if (!line)  // Sprawdzenie, czy alokacja pamięci na linię zakończyła się sukcesem.
+      return (NULL);  // Zwrot NULL w przypadku błędu alokacji.
+    if (update_buffer(buffer[fd])) // Aktualizacja bufora, usuwając odczytane znaki.
+      break ;  // Przerwanie pętli, jeśli znaleziono znak nowej linii.
+  }
+  if (line && bytes < 0)  // Sprawdzenie, czy linia istnieje i czy odczyt nastąpił błędem.
+  {
+    free(line);  // Zwolnienie pamięci zaalokowanej na linię.
+    line = NULL;  // Ustawienie wskaźnika linii na NULL.
+  }
+  return (line);  // Zwrócenie wskaźnika na linię.
 }
 
-int	main(void)
-{
-	char	*line;
-	int		i;
-	int		fd1;
-	fd1 = open("test.txt", O_RDONLY);
-	i = 1;
-	while (i != 40)
-	{
-		line = get_next_line(fd1);
-		printf("%s", line);
-		free(line);
-		i++;
-	}
-	close(fd1);
-	return (0);
-}
+
+
+// int main()
+// {
+// 	int fd;
+// 	char *line;
+
+// 	fd = open("file1.txt", O_RDONLY); // Otwarcie pliku do odczytu
+// 	if (fd == -1)
+// 	{
+// 		perror("open"); // Obsługa błędu otwarcia pliku
+// 		return 1;
+// 	}
+
+// 	while ((line = get_next_line(fd)) != NULL) // Pętla odczytująca linie z pliku
+// 	{
+// 		printf("%s\n", line); // Wyświetlenie odczytanej linii
+// 		free(line); // Zwolnienie pamięci po każdej odczytanej linii
+// 	}
+
+// 	close(fd); // Zamknięcie pliku
+// 	return 0;
+// }
