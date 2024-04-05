7389426f52dfee9fd57ad5a1476c582e4825deec
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-04-05 13:24:43 +0200

    first function

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,28 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:08:27 by mbany             #+#    #+#             */
-/*   Updated: 2024/04/05 13:08:28 by mbany            ###   ########.fr       */
+/*   Updated: 2024/04/05 13:23:59 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+ft_printf(const char *, ...)
+{
+	va_start(args, string);
+	while (string[i] !='\0')
+	{
+		if (string[i] == '%')
+		{
+			i++;
+			ft_signcheck(string[i], &args, &i);
+			i++;
+		}
+		else
+		{
+			ft_print_char((char)string[i], &len);
+			i++;
+		}
+	}
+	va_end(args);
+	return (len;)
+}
