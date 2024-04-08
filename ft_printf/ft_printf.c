4758f9093b150a38aff85fe0c4e66cb734c9d811
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-04-08 15:46:00 +0200

    add s, d

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,39 +6,109 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:08:27 by mbany             #+#    #+#             */
-/*   Updated: 2024/04/05 16:33:05 by mbany            ###   ########.fr       */
+/*   Updated: 2024/04/08 15:44:21 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+// #include "ft_printf.h"
+# include <unistd.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
 
-static void	ft_signcheck(char s, va_list *args, int *len, int *i)
+int	ft_put_c(int c)
 {
-	if (s == 'c')
-		ft_put_c(va_arg(*args, int), len);
-	else
-		(*i)--;
+	return (write(1, &c, 1));
 }
 
-ft_printf(const char *string, ...)
+size_t	ft_strlen(const char *s)
 {
-	va_list	args;
-	int	len;
-	int	i;
+	size_t	i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+int	ft_put_s(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	write(1, str, i);
+	return (1);
+}
+int	ft_put_n(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_put_c('-');
+		ft_put_c('2');
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_put_c('-');
+		nb *= -1;
+	}
+	if (nb >= 10)
+	{
+		ft_put_n(nb / 10);
+		ft_put_n(nb % 10);
+	}
+	else
+	{
+		ft_put_c(nb + '0');
+	}
+	return (1);
+}
+
+static int ft_print_arg(va_list args, int s)
+{
+	int i;
+
+	i = 0;
+	if (s == 'c')
+		i = ft_put_c(va_arg(args, int));
+	else if (s == 's')
+		i = ft_put_s(va_arg(args, char *));
+	else if (s == 'd')
+		i = ft_put_n(va_arg(args, int));
+	return (i);
+}
+
+int ft_printf(const char *string, ...)
+{
+	va_list args;
+	int len;
 
 	va_start(args, string);
+	len = 0;
 	while (*string)
 	{
 		if (*string == '%')
 		{
-			ft_signcheck(string[i], &args, &len, &i);
+			len += ft_print_arg(args, *(++string));
 		}
 		else
-		{
-			ft_print_char((char)string[i], &len);
-			i++;
-		}
+			len += ft_put_c(*string);
+		string++;
 	}
 	va_end(args);
 	return (len);
+}
+
+int main()
+{
+	char c = 'c';
+	ft_printf("%c\n", c);
+	char s[] = "sdhdsh";
+	ft_printf("%s\n", s);
+	int d = 999;
+	ft_printf("%d\n", d);
 }
