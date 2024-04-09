e362a737c3337c4317a38aba5a3abae7bae6453d
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-04-09 15:55:36 +0200

    add u

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:08:27 by mbany             #+#    #+#             */
-/*   Updated: 2024/04/09 15:25:50 by mbany            ###   ########.fr       */
+/*   Updated: 2024/04/09 15:54:51 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	ft_put_s(char *str)
 	write(1, str, i);
 	return (1);
 }
+
 static int	ft_put_d_rec(int nb, int len)
 {
 	if (nb == -2147483648)
@@ -71,6 +72,26 @@ int	ft_put_n(int n)
 {
 	return (ft_put_d_rec(n, 0));
 }
+
+static int	ft_put_u_rec(unsigned int n, int len)
+{
+ if (n <= 9)
+	{
+		len += ft_put_c(n % 10 + '0');
+	}
+	else
+	{
+		len = ft_put_u_rec(n / 10, len);
+		len += ft_put_c(n % 10 + '0');
+	}
+	return (len);
+}
+
+int	ft_put_u(unsigned int n)
+{
+	return (ft_put_u_rec(n, 0));
+}
+
 static int ft_print_arg(va_list args, int s)
 {
 	int i;
@@ -124,4 +145,6 @@ int main()
 	ft_printf("%i\n", i);
 	int perc = '%';
 	ft_printf("%%\n", perc);
+	int u = 777777777;
+	ft_printf("%u\n", u);
 }
