cead73a7293ff96ddd70273800f8ce34b0e8bbfb
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-04-15 12:38:07 +0200

    passed version

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,159 +6,15 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:08:27 by mbany             #+#    #+#             */
-/*   Updated: 2024/04/10 16:05:11 by mbany            ###   ########.fr       */
+/*   Updated: 2024/04/12 12:21:44 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "ft_printf.h"
-# include <unistd.h>
-# include <stdarg.h>
-# include <stddef.h>
-# include <stdlib.h>
+#include "ft_printf.h"
 
-int	ft_put_c(int c)
+static int	ft_print_arg(va_list args, int s)
 {
-	return (write(1, &c, 1));
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != 0)
-		i++;
-	return (i);
-}
-
-int	ft_put_s(char *str)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	write(1, str, i);
-	return (1);
-}
-
-static int	ft_put_d_rec(int nb, int len)
-{
-	if (nb == -2147483648)
-	{
-		len += ft_put_s("-2147483648");
-		return (len);
-	}
-	else if (nb < 0)
-	{
-		len += ft_put_c('-');
-		len = ft_put_d_rec(-nb, len);
-	}
-	else if (nb >= 10)
-	{
-		len = ft_put_d_rec(nb / 10, len);
-		len += ft_put_c(nb % 10 + '0');
-	}
-	else
-	{
-		len += ft_put_c(nb + '0');
-	}
-	return (len);
-}
-
-int	ft_put_n(int n)
-{
-	return (ft_put_d_rec(n, 0));
-}
-
-static int	ft_put_u_rec(unsigned int n, int len)
-{
- if (n <= 9)
-	{
-		len += ft_put_c(n % 10 + '0');
-	}
-	else
-	{
-		len = ft_put_u_rec(n / 10, len);
-		len += ft_put_c(n % 10 + '0');
-	}
-	return (len);
-}
-
-int	ft_put_u(unsigned int n)
-{
-	return (ft_put_u_rec(n, 0));
-}
-
-static int ft_put_x_rec(unsigned int n, char *hex, int len)
-{
-	if (n <= 15)
-		len += ft_put_c(hex[n % 16]);
-	else
-	{
-		len = ft_put_x_rec(n / 16, hex, len);
-		len += ft_put_c(hex[n % 16]);
-	}
-	return (len);
-}
-
-int ft_put_x(unsigned int n, int s)
-{
-	char	*hex;
-
-	if (s == 'x')
-		hex = "0123456789abcdef";
-	else
-		hex = "0123456789ABCDEF";
-	return (ft_put_x_rec(n, hex, 0));
-}
-
-int	ft_len_ptr(size_t nb)
-{
-	int	len;
-	len = 0;
-	while (nb != 0)
-	{
-		len++;
-		nb = nb / 16;
-	}
-	return (len);
-}
-void	ft_put_ptr(size_t nb)
-{
-	if (nb >= 16)
-	{
-		ft_put_ptr(nb / 16);
-		ft_put_ptr(nb % 16);
-	}
-	else
-	{
-		if (nb <= 9)
-			ft_putchar((nb + '0'));
-		else
-			ft_putchar((nb - 10 + 'a'));
-	}
-}
-int	ft_put_p(size_t ptr)
-{
-	int	ptr_print;
-	ptr_print = 0;
-	if (ptr == 0)
-		ptr_print = ptr_print + write(1, "(nil)", 5);
-	else
-	{
-		ptr_print = ptr_print + write(1, "0x", 2);
-		ft_put_ptr(ptr);
-		ptr_print = ptr_print + ft_len_ptr(ptr);
-	}
-	return (ptr_print);
-}
-
-static int ft_print_arg(va_list args, int s)
-{
-	int i;
+	int	i;
 
 	i = 0;
 	if (s == 'c')
@@ -174,14 +30,14 @@ static int ft_print_arg(va_list args, int s)
 	else if (s == 'x' || s == 'X')
 		i = ft_put_x(va_arg(args, unsigned int), s);
 	else if (s == 'p')
-		i = ft_put_p(va_arg(args, void *));
+		i = ft_put_p(va_arg(args, size_t));
 	return (i);
 }
 
-int ft_printf(const char *string, ...)
+int	ft_printf(const char *string, ...)
 {
-	va_list args;
-	int len;
+	va_list	args;
+	int		len;
 
 	va_start(args, string);
 	len = 0;
@@ -199,21 +55,21 @@ int ft_printf(const char *string, ...)
 	return (len);
 }
 
-int main()
-{
-	char c = 'c';
-	ft_printf("%c\n", c);
-	char s[] = "sdhdsh";
-	ft_printf("%s\n", s);
-	int d = 999;
-	ft_printf("%d\n", d);
-		int i = 666;
-	ft_printf("%i\n", i);
-	ft_printf("%%\n");
-	int u = 777777777;
-	ft_printf("%u\n", u);
-	int x = 15;
-	ft_printf("%x\n", x);
-	int X = 31;
-	ft_printf("%X\n", X);
-}
+// int main()
+// {
+// 	char c = 'c';
+// 	ft_printf("%c\n", c);
+// 	char s[] = "sdhdsh";
+// 	ft_printf("%s\n", s);
+// 	int d = 999;
+// 	ft_printf("%d\n", d);
+// 		int i = 666;
+// 	ft_printf("%i\n", i);
+// 	ft_printf("%%\n");
+// 	int u = 777777777;
+// 	ft_printf("%u\n", u);
+// 	int x = 15;
+// 	ft_printf("%x\n", x);
+// 	int X = 31;
+// 	ft_printf("%X\n", X);
+// }
