e3756c5d30c726ff228f958bfbf70e0f155ec88d
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-04-15 12:38:22 +0200

    passed

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,18 +6,26 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 14:15:38 by mbany             #+#    #+#             */
-/*   Updated: 2024/04/05 14:46:22 by mbany            ###   ########.fr       */
+/*   Updated: 2024/04/12 13:32:14 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
 # include <unistd.h>
 # include <stdarg.h>
 # include <stddef.h>
 # include <stdlib.h>
 
-void ft_put_c(char c, int *len);
+int			ft_put_c(int c);
+size_t		ft_strlen(const char *s);
+int			ft_put_s(char *str);
+int			ft_put_n(int n);
+int			ft_put_u(unsigned int n);
+int			ft_put_x(unsigned int n, int s);
+int			ft_len_ptr(size_t nb);
+void		ft_put_ptr(size_t nb);
+int			ft_put_p(size_t ptr);
+int			ft_printf(const char *string, ...);
 
 #endif
