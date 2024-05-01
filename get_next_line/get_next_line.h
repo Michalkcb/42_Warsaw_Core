c7d955f7e94c380855af6b74e1f519cde8f8782f
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-05-01 15:56:57 +0200

    Add files via upload

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,24 +6,25 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:02:40 by mbany             #+#    #+#             */
-/*   Updated: 2024/03/29 16:10:23 by mbany            ###   ########.fr       */
+/*   Updated: 2024/05/01 15:42:35 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
+
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int i);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
-char	*ft_strdup(char *s);
-size_t	ft_strlen(char *s);
-char	*ft_substr(char *s, unsigned int start, size_t len);
-char	*ft_strjoin(char *s1, char *s2);
-void	fill_str(char *res, char *s1, char *s2);
 
 #endif
