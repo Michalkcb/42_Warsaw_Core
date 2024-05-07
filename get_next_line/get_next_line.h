d8eb5f18fedc0e66daec309142d4fd384209d1b7
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-05-07 12:49:11 +0200

    Add files via upload

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:02:40 by mbany             #+#    #+#             */
-/*   Updated: 2024/05/01 15:42:35 by mbany            ###   ########.fr       */
+/*   Updated: 2024/05/07 11:31:05 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int i);
-char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
 
