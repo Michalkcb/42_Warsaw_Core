639d43042538c78f479b64142fa8128e34e08716
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-05-10 16:27:58 +0200

    Add files via upload

---
+++ exam/level_1/fizbuzz_ok.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizbuzz_test.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 11:50:41 by mbany             #+#    #+#             */
+/*   Updated: 2024/05/10 13:40:40 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : fizzbuzz
+Expected files   : fizzbuzz.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that prints the numbers from 1 to 100, each separated by a
+newline.
+
+If the number is a multiple of 3, it prints 'fizz' instead.
+
+If the number is a multiple of 5, it prints 'buzz' instead.
+
+If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+
+Example:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$>
+*/
+#include <unistd.h>
+
+void number(int i)
+{
+	if (i > 9)
+		number(i / 10);
+	write(1,&"0123456789"[i % 10],1);
+
+}
+
+int main(void)
+{
+	int i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			write(1,"fizzbuzz",8);
+		else if (i % 3 == 0)
+			write(1,"fizz",4);
+		else if (i % 5 == 0)
+			write(1,"buzz",4);
+		else
+			number(i);
+		i++;
+		write(1,"\n",1);
+	}
+}
