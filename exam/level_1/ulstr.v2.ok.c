c8ca3116653a0ce8d82e4b9951797a8474e89321
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-05-13 16:12:26 +0200

    program ac av

---
+++ exam/level_1/ulstr.v2.ok.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ulstr copy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 14:57:09 by mbany             #+#    #+#             */
+/*   Updated: 2024/05/13 16:07:26 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : ulstr
+Expected files   : ulstr.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and reverses the case of all its letters.
+Other characters remain unchanged.
+
+You must display the result followed by a '\n'.
+
+If the number of arguments is not 1, the program displays '\n'.
+
+Examples :
+
+$>./ulstr "L'eSPrit nE peUt plUs pRogResSer s'Il staGne et sI peRsIsTent VAnIte et auto-justification." | cat -e
+l'EspRIT Ne PEuT PLuS PrOGrESsER S'iL STAgNE ET Si PErSiStENT vaNiTE ET AUTO-JUSTIFICATION.$
+$>./ulstr "S'enTOuRer dE sECreT eSt uN sIGnE De mAnQuE De coNNaiSSanCe.  " | cat -e
+s'ENtoUrER De SecREt EsT Un SigNe dE MaNqUe dE COnnAIssANcE.  $
+$>./ulstr "3:21 Ba  tOut  moUn ki Ka di KE m'en Ka fe fot" | cat -e
+3:21 bA  ToUT  MOuN KI kA DI ke M'EN kA FE FOT$
+$>./ulstr | cat -e
+$
+*/
+#include <unistd.h>
+#include <stdint.h>
+int main(int ac, char *av[])
+{
+	while (ac == 2)
+	{
+		int i = 0;
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			{
+				av[1][i] -= 32;
+				write(1, &av[1][i], 1);
+				i++;
+			}
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			{
+				av[1][i] += 32;
+				write(1, &av[1][i], 1);
+				i++;
+			}
+		}
+	write(1,"\n",1);
+	return 0;
+	}
+}
