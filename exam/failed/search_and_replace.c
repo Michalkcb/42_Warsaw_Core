4e10eb7208796ae44fb00050eee9e1a6ad5c1430
Michael <michalkcb@gmail.com>
2024-05-16 18:47:51 +0200

    wrgrg

--- exam/failed/search_and_replace.c
+++ exam/failed/search_and_replace.c
@@ -2,28 +2,17 @@
 
 int main(int ac, char *av[])
 {
-	int i;
-	if (ac == 4 && av[2][0] && av[3][0] )
+	if (ac == 4)
 	{
+	int i;
 	i = 0;
-		if (!av[2][1])
-		{
-			if (!av[3][1])
-			{
-				while(av[1][i] != 0)
+				while(av[1][i] != 0 && av[2][1] == 0 && av[3][1] == 0)
 				{
-					if (av[1][i] != av [2][0])
-						write(1, &av[1][i],1);
 					if (av[1][i] == av[2][0])
-					{
 						av[1][i] = av[3][0];
-						write(1, &av[1][i],1);
-					}
+					write(1, &av[1][i],1);
 					i++;
 				}
-			}
 		}
-	}
 	write(1, "\n",1);
-	return (0);
 }
