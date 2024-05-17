0c10f419e2910741558e7d6287efeff0fe480dd9
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-05-17 12:23:56 +0200

    Create add_prime_sum.c

---
+++ exam/level_3/add_prime_sum.c
@@ -0,0 +1,90 @@
+// Assignment name  : add_prime_sum
+// Expected files   : add_prime_sum.c
+// Allowed functions: write, exit
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a positive integer as argument and displays the sum
+// of all prime numbers inferior or equal to it followed by a newline.
+
+// If the number of arguments is not 1, or the argument is not a positive number,
+// just display 0 followed by a newline.
+
+// Yes, the examples are right.
+
+// Examples:
+
+// $>./add_prime_sum 5
+// 10
+// $>./add_prime_sum 7 | cat -e
+// 17$
+// $>./add_prime_sum | cat -e
+// 0$
+// $>
+
+#include <unistd.h>
+
+void ft_putchar(char c);
+void ft_putnbr(int nbr);
+int is_prime(int nbr);
+int ft_atoi(char *str);
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putnbr(int nbr)
+{
+	if (nbr > 9)
+		ft_putnbr(nbr / 10);
+	write(1,&"0123456789"[nbr % 10],1);
+}
+
+int is_prime(int nbr)
+{
+    if (nbr < 2)
+        return (0);
+    int i = 2;
+    while (i <= nbr / 2)
+    {
+        if (nbr % i == 0)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+int ft_atoi(char *str)
+{
+    int res = 0, sign = 1, i = 0;
+    while (str[i] == ' ')
+        i++;
+    if (str[i] == '+' || str[i] == '-')
+        if (str[i++] == '-')
+            sign = -1;
+    while (str[i] && str[i] >= 48 && str[i] <= 57)
+    {
+        res *= 10;
+        res += str[i] - 48;
+        i++;
+    }
+    return (res * sign);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2 || ft_atoi(av[1]) <= 0)
+    {
+        write(1, "0\n", 2);
+        return (0);
+    }
+    int n = ft_atoi(av[1]);
+    int sum = 0;
+    while (n > 1)
+    {
+        if (is_prime(n))
+            sum += n;
+        n--;
+    }
+    ft_putnbr(sum);
+    ft_putchar('\n');
+}
