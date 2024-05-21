e4268d889b4718087e0c6f426718fdda960fef63
Michalkcb <michalkcb@gmail.com>
2024-05-21 20:14:50 +0200

    ?

--- exam/test/repeat_alpha.c
+++ exam/test/repeat_alpha.c
@@ -29,3 +29,38 @@ $>./repeat_alpha "" | cat -e
 $
 $>
 */
+#include <unistd.h>
+
+int main(int ac, char *av[])
+{
+    if (ac == 2)
+    {
+        int i = 0;
+        int j;
+        while (av[1][i])
+        {
+            if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+            {
+                j = 0;
+                while (j < av[1][i] - 64)
+                {
+                    write(1, &av[1][i],1);
+                    j++;
+                }
+            }
+            else if (av[1][i] >= 'a' && av[1][i] <= 'z')
+            {
+                j = 0;
+                while (j < av[1][i] - 96)
+                {
+                    write(1, &av[1][i],1);
+                    j++;
+                }
+            }
+            else
+                write(1, &av[1][i],1);
+            i++;
+        }
+    }
+    write(1, "\n",1);
+}
--- exam/test/rev_print.c
+++ exam/test/rev_print.c
@@ -17,3 +17,18 @@ LIOP a 0bud$
 $> ./rev_print | cat -e
 $
 */
+
+#include <unistd.h>
+
+int main(int ac, char *av[])
+{
+    if (ac == 2)
+    {
+        int i = 0;
+        while (av[1][i])
+            i++;
+        while (i--)
+            write(1, &av[1][i],1);
+    }
+    write (1, "\n",1);
+}
