68b26266df7022afb17a6a42e362dfb6bd0bcbbd
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-05-24 17:07:59 +0200

    working version

---
+++ exam/level_2/last_word.c
@@ -0,0 +1,58 @@
+/*
+Assignment name  : last_word
+Expected files   : last_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays its last word followed by a \n.
+
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+
+If the number of parameters is not 1, or there are no words, display a newline.
+
+Example:
+
+$> ./last_word "FOR PONY" | cat -e
+PONY$
+$> ./last_word "this        ...       is sparta, then again, maybe    not" | cat -e
+not$
+$> ./last_word "   " | cat -e
+$
+$> ./last_word "a" "b" | cat -e
+$
+$> ./last_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+
+int main(int ac, char *av[])
+{
+	if (ac == 2)
+	{
+	int	j;
+	int i;
+
+	i = 0;
+	j = 0;
+	while (av[1][i])
+	{
+		if (av[1][i] == ' ' && av[1][i + 1] >= 33 && av[1][i + 1] <= 126)
+			j = i + 1;
+		i++;
+	}
+	while (av[1][j] >= 33 && av[1][j] <= 127)
+	{
+		write(1, &av[1][j], 1);
+		j++;
+	}
+	
+
+		
+	}
+	write(1, "\n",1);
+}
