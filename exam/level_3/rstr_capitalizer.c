11aa1af58dc0b3d0f501dbfb99bb85b755956006
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-06-07 19:21:22 +0200

    working :)

--- exam/level_3/rstr_capitalizer.c
+++ exam/level_3/rstr_capitalizer.c
@@ -29,30 +29,30 @@ $>
 */
 #include <unistd.h>
 
-void rstr_capitalizer(char *str)
-{
-  int i = 0;
-  while (str[i])
-  {
-    if (str[i] >= 'A' && str <= 'Z')
-      str += 32;
-  }
-}
-
+#include <unistd.h>
 
 int main(int ac, char *av[])
 {
-  int i;
+  int i, j;
+  
   if (ac == 1)
-    write(1,"\n", 1);
+    write(1, "\n", 1);
   else
   {
-    i = 0;
+    i = 1; // Start from 1 to skip the program name
     while (i < ac)
     {
-      rstr_capitalizer(av[i]);
-      write(1,"\n", 1);
-      i+= 1;
+      j = 0;
+      while (av[i][j])
+      {
+        if (av[i][j] >= 'A' && av[i][j] <= 'Z')
+          av[i][j] += 32;
+        if ((av[i][j] >= 'a' && av[i][j] <= 'z') && (av[i][j + 1] == ' ' || av[i][j + 1] == '\t' || av[i][j + 1] == '\0'))
+          av[i][j] -= 32;
+        write(1, &av[i][j++], 1);
+      }
+      write(1, "\n", 1);
+      i += 1;
     }
   }
   return 0;
