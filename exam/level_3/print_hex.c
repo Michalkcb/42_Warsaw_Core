15ac089d77502ee3e75ea7d77a8a967b22b9bcbf
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-09-07 14:57:51 +0200

    add printhex

--- exam/level_3/print_hex.c
+++ exam/level_3/print_hex.c
@@ -22,31 +22,32 @@ $
 */
 
 #include <unistd.h>
-
 int ft_atoi(char *s)
 {
 	int n = 0;
 	while (*s)
 	{
 		n = n * 10;
-		n = n + *s -'0';
-		s++;
+		n = s + *s -'0';
+		++s;
 	}
 	return (n);
+
 }
 
 void print_hex(int n)
 {
-	char hex_d[] = "0123456789abcdef";
-	if (n >=16)
-		print_hex(n/16);
-	write(1,&hex_d[n % 16], 1);
+	char hex_digits[] = "0123456789abcdf";
+	if (n >= 16)
+		print_hex( n / 16);
+	write(1,&hex_digits[n % 16],1);
+
 }
 
-int main(int ac, char *av[])
+int main ( int ac, char *av[])
 {
 	if (ac == 2)
 		print_hex(ft_atoi(av[1]));
-	write(1,"\n", 1);
-
+	write (1,"\n", 1);
+	return 0;
 }
