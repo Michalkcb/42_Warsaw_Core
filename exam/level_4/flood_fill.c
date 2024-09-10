2e305125ce27753826794f752ecc1124f7479d4e
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-09-10 18:55:56 +0200

    add flood

---
+++ exam/level_4/flood_fill.c
@@ -0,0 +1,148 @@
+/*
+Assignment name  : flood_fill
+Expected files   : *.c, *.h
+Allowed functions: -
+--------------------------------------------------------------------------------
+
+Write a function that takes a char ** as a 2-dimensional array of char, a
+t_point as the dimensions of this array and a t_point as the starting point.
+
+Starting from the given 'begin' t_point, this function fills an entire zone
+by replacing characters inside with the character 'F'. A zone is an group of
+the same character delimitated horizontally and vertically by other characters
+or the array boundry.
+
+The flood_fill function won't fill diagonally.
+
+The flood_fill function will be prototyped like this:
+  void  flood_fill(char **tab, t_point size, t_point begin);
+
+The t_point structure is prototyped like this:
+
+  typedef struct  s_point
+  {
+    int           x;
+    int           y;
+  }               t_point;
+
+Example:
+
+$> cat test.c
+#include <stdlib.h>
+#include <stdio.h>
+#include "flood_fill.h"
+
+char** make_area(char** zone, t_point size)
+{
+	char** new;
+
+	new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+
+	return new;
+}
+
+int main(void)
+{
+	t_point size = {8, 5};
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+
+	char**  area = make_area(zone, size);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	printf("\n");
+
+	t_point begin = {7, 4};
+	flood_fill(area, size, begin);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	return (0);
+}
+
+$> gcc flood_fill.c test.c -o test; ./test
+11111111
+10001001
+10010001
+10110001
+11100001
+
+FFFFFFFF
+F000F00F
+F00F000F
+F0FF000F
+FFF0000F
+$>
+
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct s_point
+{
+    int x;
+    int y;
+} t_point;
+
+void fill(char **tab, t_point size, t_point cur, char to_fill)
+{
+    if (cur.y < 0 || cur.x < 0 || cur.x >= size.x || cur.y >= size.y || tab[cur.y][cur.x] != to_fill)
+        return;
+    tab[cur.y][cur.x] = 'F';
+    fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
+    fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
+    fill(tab, size, (t_point){cur.x, cur.y - 1}, to_fill);
+    fill(tab, size, (t_point){cur.x, cur.y + 1}, to_fill);
+}
+
+
+void flood_fill(char **tab, t_point size, t_point begin)
+{
+    fill(tab, size, begin, tab[begin.y][begin.x]);
+}
+
+int main(void)
+{
+    // Definicja tablicy
+    char zone[5][9] = {
+        "11111111",
+        "10001001",
+        "10010001",
+        "10110001",
+        "11100001"
+    };
+
+    t_point size = {8, 5}; // Rozmiar: szerokość 8, wysokość 5
+
+    // Ustawienie wskaźników do wierszy tablicy
+    char *area[5];
+    for (int i = 0; i < size.y; i++) {
+        area[i] = zone[i];
+    }
+
+    // Wypisanie tablicy przed flood_fill
+    for (int i = 0; i < size.y; i++)
+        printf("%s\n", area[i]);
+    printf("\n");
+
+    // Punkt początkowy dla flood_fill
+    t_point begin = {7, 4}; // Zwróć uwagę na (x, y) i sprawdź, czy mieści się w tablicy
+    flood_fill(area, size, begin);
+
+    // Wypisanie tablicy po flood_fill
+    for (int i = 0; i < size.y; i++)
+        printf("%s\n", area[i]);
+
+    return 0;
+}
