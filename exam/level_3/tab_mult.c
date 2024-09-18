84b1cb140d8a08db698e8d2274e5c3dd122a35f8
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-09-18 23:48:24 +0200

    add tab_mult

---
+++ exam/level_3/tab_mult.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	write(1, &"0123456789"[nb % 10],1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	nbr;
+
+	if (argc == 2)
+	{
+		i = 1;
+		nbr = ft_atoi(argv[1]);
+		while (i <= 9 && nbr <= 238609183)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(nbr);
+			write(1, " = ", 3);
+			ft_putnbr(i * nbr);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
