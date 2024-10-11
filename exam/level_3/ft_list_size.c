427c4d67b51894d268dbca53514aa95933ff9767
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-10-11 20:37:13 +0200

    Update ft_list_size.c

--- exam/level_3/ft_list_size.c
+++ exam/level_3/ft_list_size.c
@@ -21,34 +21,26 @@ typedef struct    s_list
 }                 t_list;
 */
 #include "ft_list.h"
+
 int	ft_list_size(t_list *begin_list)
 {
 	int i = 0;
-	while (begin_list)
+	while(begin_list)
 	{
 		begin_list = begin_list -> next;
 		i++;
 	}
-	return(i);
+	return (i);
 }
+
 #include <stdio.h>
 int main ()
 {
-	// t_list el2 = {NULL, "el1"};
-	// t_list el1 = {&el2, "el1"};
-	// t_list *list = &el1;
-
 	t_list node1, node2, node3, node4;
-	node1.data = "1";
-	node2.data = "2";
-	node3.data = "3";
-	node4.data = "4";
-	t_list *list = &node1;
-
 	node1.next = &node2;
 	node2.next = &node3;
 	node3.next = &node4;
 	node4.next = NULL;
-
-	printf("%d\n", ft_list_size(list));	
+	t_list *list = &node1;
+	printf("%d\n", ft_list_size(list));
 }
