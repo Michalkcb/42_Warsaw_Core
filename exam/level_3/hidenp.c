90a60224bd9e9b3daef35b630741e07663d430db
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-10-11 20:29:47 +0200

    Update hidenp.c

--- exam/level_3/hidenp.c
+++ exam/level_3/hidenp.c
@@ -27,26 +27,26 @@ $
 $>
 */
 
-# include <unistd.h>
-
-int main(int ac, char *av[])
+#include <unistd.h>
+int main(int ac, char **av)
 {
-	int i = 0;
-	int j = 0;
-	if (ac == 3)
+	if(ac == 3)
 	{
-		while (av[2][j] && av[1][i])
+		int i = 0;
+		int j = 0;
+		char *s1 = av[1];
+		char *s2 = av[2];
+		while(s1[i] && s2[j])
 		{
-			if (av[2][j] == av[1][i])
+			if(s1[i] == s2[j])
 				i++;
 			j++;
 		}
-		if (av[1][i] == '\0')
-			write(1, "1", 1);
+		if(s1[i] == 0)
+			write(1, "1",1);
 		else
-			write(1, "0", 1);
-
+			write(1, "0",1);
 	}
-	write(1,"\n", 1);	
+	write(1, "\n",1);
 	return 0;
 }
