4967f6f67d874e89bb8f85f9f9a0a92985a50b83
Michael <michalkcb@gmail.com>
2024-10-16 23:55:51 +0200

    itoa ok

--- exam/level_4/!ft_itoa.c
+++ exam/level_4/!ft_itoa.c
@@ -13,51 +13,91 @@ char	*ft_itoa(int nbr);
 
 */
 #include <stdlib.h>
-
 char	*ft_itoa(int nbr)
 {
-	if (nbr == -2147483648)
+	if(nbr == -2147483648)
 		return("-2147483648\0");
+	 int len = 0;
+	 int n = nbr;
 
-	int n = nbr;
-	int len = 0;
-
-	if(nbr <= 0)
+	if(nbr < 0)
 		len++;
-
 	while(n)
-		{
-			n /= 10;
-			len++;
-		}
+	{
+		n /=10;
+		len++;
+	}
+	char *res = (char *)malloc(sizeof(char)*(len + 1));
 
-	char *res = (char *)malloc(sizeof(char) * (len +1));
-	if ( res == NULL)
+	if(res == NULL)
 		return NULL;
 	res[len] = '\0';
 
-	if (nbr == 0)
-	{
-		res[0] = '0';
-		return (res);
-	}
-	if (nbr < 0)
+	if(nbr == 0)
+		{
+			res[0] = '\0';
+			return(res);
+		}
+	if(nbr < 0)
 	{
 		res[0] = '-';
-		nbr = -nbr;
+		nbr=-nbr;
 	}
 	while (nbr)
 	{
-		res[--len] = nbr %10 +'0';
-		nbr /= 10;
+		res[--len] = nbr % 10 + '0';
+		nbr /=10;
 	}
 	return res;
-	
+
 }
+
+
+// char	*ft_itoa(int nbr)
+// {
+// 	if (nbr == -2147483648)
+// 		return("-2147483648\0");
+
+// 	int n = nbr;
+// 	int len = 0;
+
+// 	if(nbr <= 0)
+// 		len++;
+
+// 	while(n)
+// 		{
+// 			n /= 10;
+// 			len++;
+// 		}
+
+// 	char *res = (char *)malloc(sizeof(char) * (len +1));
+
+// 	if(res == NULL)
+// 		return NULL;
+// 	res[len] = '\0';
+
+// 	if (nbr == 0)
+// 	{
+// 		res[0] = '0';
+// 		return (res);
+// 	}
+// 	if (nbr < 0)
+// 	{
+// 		res[0] = '-';
+// 		nbr = -nbr;
+// 	}
+// 	while (nbr)
+// 	{
+// 		res[--len] = nbr %10 +'0';
+// 		nbr /= 10;
+// 	}
+// 	return res;
+	
+// }
 #include <stdio.h>
 int main ()
 {
-	int nbr = 19;
-	printf("%s", ft_itoa(nbr));
+	int nbr = -19;
+	printf("%s\n", ft_itoa(nbr));
 
 }
